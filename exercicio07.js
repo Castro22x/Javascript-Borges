e14516ce227fc5543ba1656f32e7a97591f1374d
Castro22x <gabrielmachado224455@gmail.com>
2025-11-18 13:47:15 -0300

    Exercicio 07

---
+++ exercicio07.js
@@ -0,0 +1,16 @@
+let valor = Number(prompt("Digite o valor total da compra: "));
+let desconto = 0;
+
+if (valor <= 100) {
+    desconto = 0;
+} else if (valor <= 200) {
+    desconto = valor * 0.10;  // 10%
+} else {
+    desconto = valor * 0.20;  // 20%
+}
+
+let valorFinal = valor - desconto;
+
+console.log("Preço original:", valor);
+console.log("Valor do desconto:", desconto);
+console.log("Preço final:", valorFinal);
