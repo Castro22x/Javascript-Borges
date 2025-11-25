4e919ad4d17948b7767e0ee998fbf19ffaa6d2b8
Castro22x <gabrielmachado224455@gmail.com>
2025-11-25 14:16:11 -0300

    Add files via upload

---
+++ Exercicio08.js
@@ -0,0 +1,10 @@
+
+
+let idade = parseInt(prompt("Digite sua idade: "));
+
+while (idade < 18) {
+    console.log("Você é menor de idade. Tente novamente.");
+    idade = parseInt(prompt("Digite sua idade: "));
+}
+
+console.log("Você é maior de idade: " + idade);
